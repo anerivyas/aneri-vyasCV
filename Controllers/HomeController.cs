4d77e351d10cf83c73b51611d9cde0b05a268fe7
anerivyas <virus1996@hotmail.co.nz>
2020-08-11 18:25:17 +1200

    Initial check-in of module aneri-vyas

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using aneri_vyas.Models;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace aneri_vyas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            MainModel MainModel = new MainModel();
+            List<User> User = new List<User>();
+
+            MainModel.User = User;
+
+            dynamic UserData = JsonConvert.DeserializeObject<dynamic>(System.IO.File.ReadAllText("Database/User.json"));
+
+
+            foreach (dynamic item in UserData)
+            {
+                User.Add(new User
+                {
+                    UserID = (int)item["UserID"],
+                    Name = (string)item["Name"],
+                    Title = (string)item["Title"]
+                });
+
+
+            }
+
+
+            List<Skills> Skills = new List<Skills>();
+            MainModel.Skills = Skills;
+
+            dynamic SkillsData = JsonConvert.DeserializeObject<dynamic>(System.IO.File.ReadAllText("Database/Skills.json"));
+
+
+            foreach (dynamic item in SkillsData)
+            {
+                Skills.Add(new Skills
+                {
+                    UserID = (int)item["UserID"],
+                    Name = (string)item["Name"],
+                    SkillID = (int)item["SkillID"],
+                    Type = (string)item["Type"],
+                    YearsOfExperience = (string)item["YearsOfExperience"],
+                    Description = (string)item["Description"]
+                }); 
+            }
+            
+
+
+            return View(MainModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/AppModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aneri_vyas.Models
+{
+    public class MainModel
+    {
+        public List<User> User { get; set; }
+        public List<Skills> Skills { get; set; }
+
+    }
+
+
+    public class User
+    {
+        public int UserID { get; set;}
+        public string Name { get; set; }
+        public string Title { get; set;}
+    }
+
+    public class Skills
+    {
+        public int SkillID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string YearsOfExperience { get; set; }
+        public string Description { get; set; }
+    }
+}
+
